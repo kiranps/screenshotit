245d3479b8c01ca76a05079fa7f87fa03f0d5a6b
Kiran PS <pskirann@gmail.com>
2018-10-11 17:09:00 +0530

    upgraded gatsby

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import Logo from '../Logo'
-import Link from 'gatsby-link'
+import { Link } from 'gatsby'
 
 const Header = styled.header`
   background-color: #fff;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,21 @@
 import React from 'react'
-import Link from 'gatsby-link'
+import Helmet from 'react-helmet'
+import Header from '../components/Header'
+import ProjectIntro from '../components/ProjectIntro'
+import Footer from '../components/Footer'
 
 const IndexPage = () => (
   <div>
+    <Helmet
+      title="ScreenshotIt"
+      meta={[
+        { name: 'description', content: 'Sample' },
+        { name: 'keywords', content: 'sample, something' },
+      ]}
+    />
+    <Header />
+    <ProjectIntro />
+    <Footer/>
   </div>
 )
 
